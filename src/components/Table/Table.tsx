9f605c08919158bd6cd5eb041ac1cbc85cad80f2
Anthony Lee <42955348+anthonylee2797@users.noreply.github.com>
2024-08-30 21:14:36 -0400

    Update Table.tsx

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,71 +1,67 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button"; // Import the Button component
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button'; // Import the Button component
 
-import "./table.css";
-
-
+import './Table.css';
 
 const styles = {
   TableContainer: {
-    width: "80%",
-    margin: "auto",
-    marginTop: "20px",
-    background: "#e0fbfc",
+    width: '80%',
+    margin: 'auto',
+    marginTop: '20px',
+    background: '#e0fbfc',
   },
   TableHead: {
-    background: "#ee6c4d",
-    fontWeight: "bold",
-    borderBottom: "3px solid black",
+    background: '#ee6c4d',
+    fontWeight: 'bold',
+    borderBottom: '3px solid black',
   },
   TableHeadCell: {
-    fontWeight: "bold",
-    fontSize: "16px",
+    fontWeight: 'bold',
+    fontSize: '16px',
   },
 };
 
 export default function TableComponent({ rows }) {
-  console.log(rows, 'rows')
+  console.log(rows, 'rows');
   return (
     <TableContainer sx={styles.TableContainer} component={Paper}>
-      <Table size="small" sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table size='small' sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
           <TableRow sx={styles.TableHead}>
             <TableCell sx={styles.TableHeadCell}>Student</TableCell>
-            <TableCell sx={styles.TableHeadCell} align="right">
+            <TableCell sx={styles.TableHeadCell} align='right'>
               50y Freestyle
             </TableCell>
-            <TableCell sx={styles.TableHeadCell} align="right">
+            <TableCell sx={styles.TableHeadCell} align='right'>
               50y Backstroke
             </TableCell>
-            <TableCell sx={styles.TableHeadCell} align="right">
+            <TableCell sx={styles.TableHeadCell} align='right'>
               50y Breastroke
             </TableCell>
-            <TableCell sx={styles.TableHeadCell} align="right">
+            <TableCell sx={styles.TableHeadCell} align='right'>
               50y Butterfly
             </TableCell>
-           
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {rows.map(row => (
             <TableRow
               key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
+              <TableCell component='th' scope='row'>
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.freestyle}</TableCell>
-              <TableCell align="right">{row.backstroke}</TableCell>
-              <TableCell align="right">{row.breastroke}</TableCell>
-              <TableCell align="right">{row.butterfly}</TableCell>
-          
+              <TableCell align='right'>{row.freestyle}</TableCell>
+              <TableCell align='right'>{row.backstroke}</TableCell>
+              <TableCell align='right'>{row.breastroke}</TableCell>
+              <TableCell align='right'>{row.butterfly}</TableCell>
             </TableRow>
           ))}
         </TableBody>
