58379ccbd6b6d420956d0c8a8a1e2186ea352289
Anthony Lee <42955348+anthonylee2797@users.noreply.github.com>
2024-10-06 12:51:03 -0400

    Update firebase.ts

--- src/server/firebase/firebase.ts
+++ src/server/firebase/firebase.ts
@@ -9,13 +9,13 @@ import { getTodaysDate } from '../../utils/date.ts';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: 'swimming-472cb.firebaseapp.com',
-  projectId: 'swimming-472cb',
-  storageBucket: 'swimming-472cb.appspot.com',
-  messagingSenderId: '1072230841035',
-  appId: '1:1072230841035:web:45bd9a7cc9de4d40c7f94e',
-  measurementId: 'G-0YDYRJNPSJ',
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
