8e09687adb2fdc7765dd12c3ca8308076333ad3f
Anthony Lee <42955348+anthonylee2797@users.noreply.github.com>
2024-10-24 16:35:10 -0400

    updates

--- src/server/firebase/constants.ts
+++ src/server/firebase/constants.ts
@@ -1,2 +1,4 @@
 export const SWIM_CLOUD = 'swim-cloud';
 export const NYC_50_FREE_2023_2024 = 'NYC-50-FREE-2023-2024';
+export const MIDDLESCHOOL_50_FREE_2023_2024 = 'MIDDLESCHOOL-50-FREE-2023-2024';
+export const MIDDLESCHOOL = `https://www.swimcloud.com/country/usa/org/mss/times/?age_group&dont_group=false&event=150&event_course=S&gender=M&page=1&region=countryorganisation_MSS&season_id=27&team_id&year`;
