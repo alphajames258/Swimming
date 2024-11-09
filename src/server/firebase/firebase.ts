1f40bb12cb3d036b39fe52b36e8a7c73a37a4c69
Anthony Lee <42955348+anthonylee2797@users.noreply.github.com>
2024-11-09 18:29:52 -0500

    local branch stuff

--- src/server/firebase/firebase.ts
+++ src/server/firebase/firebase.ts
@@ -1,6 +1,3 @@
-// Import the functions you need from the SDKs you need
-import '../../../envConfig.ts';
-
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,22 +6,31 @@ import { getFirestore, setDoc, doc, getDoc } from 'firebase/firestore';
 import { getTodaysDate } from '../../utils/date.ts';
 
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseConfig } from '../../../local.ts';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+(async () => {
+  const uid = await signIn('anthonylee2797@gmail.com', 'test123');
+  if (uid) {
+    console.log('Authenticated user UID:', uid);
+    // Now you can use the UID for user-specific operations
+  }
+})();
 
 export async function signIn(email: string, password: string) {
   try {
@@ -41,13 +47,6 @@ export async function signIn(email: string, password: string) {
 }
 
 // Example usage
-(async () => {
-  const uid = await signIn('anthonylee2797@gmail.com', 'test123');
-  if (uid) {
-    console.log('Authenticated user UID:', uid);
-    // Now you can use the UID for user-specific operations
-  }
-})();
 
 const firestore = getFirestore(app);
 
